04b34c1d27d7d000c91b9239c6a1c1cf9f46132f
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-05-23 09:36:39 +0700

    Add Favicon

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ $result = $conn->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TugasKu</title>
 
+  <link rel="icon" href="img/favicon.png" type="img/png" />
+
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
