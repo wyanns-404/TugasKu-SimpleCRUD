d15ed9dc1f08d152613ad06b964e7f0e672a5fbd
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-05-23 09:41:01 +0700

    Update Error Alert

--- db/db.php
+++ db/db.php
@@ -7,6 +7,6 @@ $dbname = "db_tugasku";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Koneksi Gagal: " . $conn->connect_error);
 }
 ?>
